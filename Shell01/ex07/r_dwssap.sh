1e9ad500c10960c64be66da85a6dd877c80ae101
Chanha Lee <chanhale@c3r7s7.42seoul.kr>
2021-09-14 12:01:04 +0900

    202109141200

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '^#' | awk -F: 'NR>=2 {print $1}' | rev | sort -r | awk -v a=$FT_LINE1 -v b=$FT_LINE2 'NR>=a && NR<=b {print $0","}'| tr '\n' ' ' | sed 's/, $/./' | tr -d ' '
+cat /etc/passwd | grep -v '^#' | awk -F: 'NR>=2 {print $1}' | rev | sort -r | awk -v a=$FT_LINE1 -v b=$FT_LINE2 'NR>=a && NR<=b {print $0","}'| tr '\n' ' ' | sed 's/, $/./' | tr -d '\n'
