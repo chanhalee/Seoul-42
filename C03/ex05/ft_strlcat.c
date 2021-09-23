c6afd72cb07a5e0822ed780fd6c8d97c5e211229
lch <chan771788@gmail.com>
2021-09-24 01:10:47 +0900

    C03 ex05 한번 볼것

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: chanhale <chanhale@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 17:54:33 by chanhale          #+#    #+#             */
-/*   Updated: 2021/09/21 00:05:43 by chanhale         ###   ########.fr       */
+/*   Updated: 2021/09/23 20:58:28 by chanhale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	destlength;
 	unsigned int	destlencpy;
 	unsigned int	srclength;
+	unsigned int	len;
 
 	srclength = 0;
 	destlength = 0;
+	len = 0;
 	while (dest[destlength] != '\0')
 		destlength++;
-	destlencpy = destlength;
-	while (src[srclength] != '\0' && destlencpy++ < size - 1)
-	{
-		dest[destlength + srclength] = src[srclength];
-		srclength++;
-	}
-	dest[destlength + srclength] = '\0';
-	srclength = 0;
 	while (src[srclength])
 		srclength++;
-	if (size < destlength)
-		return (size + srclength);
-	return (destlength + srclength);
+	if (size == 0)
+		return (srclength);
+	while (destlength < size - 1 && src[len])
+	{
+		dest[destlength++] = src[len++];
+		srclength++;
+	}
+	dest[destlength] = '\0';
+	return (srclength);
 }
