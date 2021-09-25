a4f3d2f2747e685d27e0cd1bdf1ba1c39a53d3d0
lee-chanah <chan771788@gmail.com>
2021-09-25 14:46:14 +0900

    a

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -1,35 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chanhale <chanhale@student.42seoul.>       +#+  +:+       +#+        */
+/*   By: chanhale <chahale@student.42seoul.kr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/23 17:42:54 by chanhale          #+#    #+#             */
-/*   Updated: 2021/09/25 00:37:29 by chanhale         ###   ########.fr       */
+/*   Created: 2021/09/25 00:38:43 by chanhale          #+#    #+#             */
+/*   Updated: 2021/09/25 00:47:38 by chanhale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-unsigned int	get_gap(int min, int max);
+int	get_gap(int min, int max);
 
-int	*ft_range(int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
-	unsigned int	size;
-	int				*result;
+	int	size;
+	int	index;
 
 	size = get_gap(min, max);
 	if (size == 0)
-		return (NULL);
-	result = (int *)malloc(sizeof(int) * size);
-	size = -1;
+	{
+		range = NULL;
+		return (0);
+	}
+	*range = (int *)malloc(sizeof(int) * size);
+	if (range == NULL)
+		return (0);
+	index = -1;
 	while (min != max)
-		result[++size] = min++;
-	return (result);
+		(*range)[++index] = min++;
+	return (size);
 }
 
-unsigned int	get_gap(int min, int max)
+int	get_gap(int min, int max)
 {
 	unsigned int	result;
 
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanhale <chahale@student.42seoul.kr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 00:50:34 by chanhale          #+#    #+#             */
+/*   Updated: 2021/09/25 02:41:58 by chanhale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		assune_len(int size, char **strs, char *sep);
+char	*ft_strcat(char *dest, char *src);
+
+char *ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*result;
+	int		len;
+
+	len = assune_len(size, strs, sep);
+	result = (char *)malloc(sizeof(char) * len);
+	if (size <= 0)
+		return (result);
+	len = -1;
+	while (++len < size - 1)
+	{
+		ft_strcat(result, strs[len]);
+		ft_strcat(result, sep);
+	}
+	ft_strcat(result, strs[len]);
+	return (result);
+}
+
+int	assune_len(int size, char **strs, char *sep)
+{
+	int	result;
+	int indexa;
+	int indexb;
+
+	if (size <=0)
+		return (1);
+	result = 0;
+	indexa = -1;
+	while (++indexa < size)
+	{
+		indexb = -1;
+		while (strs[indexa][++indexb])
+			result++;
+	}
+	indexa = 0;
+	while (sep[indexa])
+		indexa++;
+	result += indexa * (size - 1) + 1;
+	return (result);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	destlength;
+	int	srclength;
+
+	srclength = 0;
+	destlength = 0;
+	while (dest[destlength] != '\0')
+		destlength++;
+	dest[destlength] = 48;
+	while (src[srclength] != '\0')
+	{
+		dest[destlength + srclength] = src[srclength];
+		srclength++;
+	}
+	dest[destlength + srclength] = '\0';
+	return (dest);
+}
