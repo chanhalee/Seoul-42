0ce52e13be93dac85b8a0a4a9238fe6694df863c
chanhalee <chan771788@gmail.com>
2022-07-04 17:13:46 +0900

    so_long bonus fin

--- 42Cursus/so_long/bonus/enemy_util_bonus.c
+++ 42Cursus/so_long/bonus/enemy_util_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chanhale <chanhale@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:48:11 by chanhale          #+#    #+#             */
-/*   Updated: 2022/07/03 21:32:15 by chanhale         ###   ########.fr       */
+/*   Updated: 2022/07/04 17:13:37 by chanhale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	check_enemy_capture(t_mlx_data *mlx)
 	if (check_matching_entry(mlx->map.enemies, mlx->map.user.pos.x,
 			mlx->map.user.pos.y) == TYPE_TRUE)
 	{
-		write(1, "\n********************\n*******CAPTURED******\n\n", 45);
+		write(1, "\n********************\n******CAPTURED******\n\n", 44);
 		write(1, "move_count:", 11);
 		ft_putnbr_custom(mlx->map.movement_count);
 		write(1, "\n", 1);
