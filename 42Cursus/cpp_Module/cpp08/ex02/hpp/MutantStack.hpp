e71e6b2204572060f611d9476d1cc44b2d017200
chanhale <chan771788@gmail.com>
2022-08-27 17:12:06 +0900

    finish

--- 42Cursus/cpp_Module/cpp08/ex02/hpp/MutantStack.hpp
+++ 42Cursus/cpp_Module/cpp08/ex02/hpp/MutantStack.hpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <exception>
 #include <vector>
+#include <deque>
 #include <stack>
 
 using std::cout;
@@ -16,8 +17,18 @@ using std::stack;
 template <typename T>
 class MutantStack: public std::stack<T>
 {
+private:
 public:
-	
-}
+	MutantStack(){}
+	MutantStack(const MutantStack &ms){(*this) = ms; return (*this);}
+	MutantStack &operator=(const MutantStack &ms){(*this) = ms; return (*this);}
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	iterator begin() {return (this->c.begin());}
+	iterator end() {return (this->c.end());}
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	reverse_iterator rbegin() {return (this->c.rbegin());}
+	reverse_iterator rend() {return (this->c.rend());}
+	~MutantStack(){}
+};
 
 #endif
